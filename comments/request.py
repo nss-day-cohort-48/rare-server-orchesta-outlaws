b4589ee1759f5ab1fb32157d112801bb0ee6bae8
S-L-Murphey <smurphey1990@gmail.com>
2021-07-26 15:29:59 -0500

    Completed function to create a new comment (not yet adddressed in request_handler)
    ..

--- comments/request.py
+++ comments/request.py
@@ -1,6 +1,22 @@
+import sqlite3
+import json
 
 def create_comment(new_comment):
     '''Reader/Author can add a comment to an author's post'''
+    with sqlite3.connect("./rare.db") as conn:
+        db_cursor = conn.cursor()
+
+        db.cursor.execute("""
+        INSERT INTO Comments
+            ( post_id, author_id, content )
+        VALUES
+            ( ?, ?, ?);
+        """, (new_comment['post_id'], new_comment['author_id'], new_comment['content'], )) 
+
+        id = db_cursor.lastrowid
+        new_comment['id'] = id
+
+    return json.dumps(new_comment)
 
 def view_post_comments(postID):
     '''Reader can see a list of all the comments on a post'''

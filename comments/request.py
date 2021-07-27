b5d385606eb5653e93fda956779b2c73a784fbc3
S-L-Murphey <smurphey1990@gmail.com>
2021-07-27 15:00:29 -0500

    Updating Comments Functionality

--- comments/request.py
+++ comments/request.py
@@ -1,7 +1,30 @@
 import sqlite3
 import json
-from models import Comment
+from models import Comment, Post
 from datetime import datetime
+
+def get_all_comments():
+    with sqlite3.connect("./rare.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.post_id,
+            c.author_id,
+            c.content,
+            p.title comment_subject
+        FROM Comments c
+        JOIN Posts p
+            ON p.id = c.post_id
+        """)
+
+        comments = []
+        dataset = db_cursor.fetchall()
+        for row in dataset:
+            comment = Comment(row['id'], row['post_id'], row['author_id'], row['content'])
+            subject = Post(row['comment_subject'])
 
 def create_comment(new_comment):
     '''Reader/Author can add a comment to an author's post. Resulting comment will display the article title and content'''

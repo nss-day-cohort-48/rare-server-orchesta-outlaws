9a6d9a34ff12536d94ccea6692f8a73ee20e665b
Val Currie <contact@valcuri.email>
2021-07-29 14:01:52 -0500

    moved sub post function to posts.request

--- posts/request.py
+++ posts/request.py
@@ -35,3 +35,17 @@ def get_posts_by_user(id):
             posts.append(post.__dict__)
 
     return posts
+
+def get_user_subbed_posts(id):
+    """Returns a list of user_ids that the user for the provided id is subbed to."""
+    with sqlite3.connect(DB_FILE) as conn:
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+            SELECT s.author_id, p.*
+            FROM Subscriptions s
+            WHERE s.follower_id = ?
+            JOIN Posts p
+            ON p.user_id = s.author_id
+        """, (id,))
+    
+        dataset = db_cursor.fetchall()
